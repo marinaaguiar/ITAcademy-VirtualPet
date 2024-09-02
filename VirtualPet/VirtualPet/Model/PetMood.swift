81b47a5db37beec1b82caac663edecc8b576ba2f
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-09-02 15:40:56 +0200

    add PetModd Model

---
+++ VirtualPet/VirtualPet/Model/PetMood.swift
@@ -0,0 +1,15 @@
+//
+//  PetMood.swift
+//  VirtualPet
+//
+//  Created by Marina Aguiar on 8/30/24.
+//
+
+import Foundation
+
+enum PetMood: String {
+    case happy = "Happy"
+    case sad = "Sad"
+    case excited = "Excited"
+    case tired = "Tired"
+}
